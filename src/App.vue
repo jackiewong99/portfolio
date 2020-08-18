077bd84dc1c8d7c3cd5b14ee9124968a14f541f3
Jackie Wong <wongjackie888@gmail.com>
2020-08-17 21:12:10 -1000

    Create responsive navbar.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,59 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <!-- NAV BAR -->
+    <nav class="nav">
+      <div class="logo">
+        Logo
+      </div>
+      <ul ref="nav" class="nav-links">
+        <li>
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li><a class="nav-link" href="#projects">Projects</a></li>
+        <li><a class="nav-link" href="#contact">Contact</a></li>
+        <li><a class="nav-link" href="#">Resume</a></li>
+      </ul>
+      <div v-on:click="navSlide" class="burger" ref="burger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+      </div>
+    </nav>
+    <!-- **RESUME OPENS NEW TAB -->
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- HOME -->
+    <!-- ABOUT -->
+    <!-- PROJECTS -->
+    <!-- CONTACT -->
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    navSlide() {
+      const burger = this.$refs.burger;
+      const nav = this.$refs.nav;
+      const navLinks = this.$refs.nav.children;
+
+      // Burger animation
+      burger.classList.toggle('toggle');
+
+      nav.classList.toggle('nav-active');
+
+      // Animate links
+      navLinks.forEach((link, index) => {
+        if (link.children[0].style.animation) {
+          link.children[0].style.animation = '';
+        } else {
+          link.children[0].style.animation = `navLinkFade 0.5s ease forwards ${index /
+            7 +
+            0.5}s`;
+        }
+      });
+    }
+  }
 };
 </script>
 
@@ -20,5 +69,114 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+nav {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  min-height: 8vh;
+}
+
+.logo {
+  font-size: 20px;
+  letter-spacing: 5px;
+}
+
+.nav-links {
+  display: flex;
+  list-style: none;
+  justify-content: space-around;
+  width: 35%;
+}
+
+.nav-link {
+  color: #2c3e50;
+  cursor: pointer;
+  font-size: 14px;
+  letter-spacing: 3px;
+  text-decoration: none;
+  transition: color 0.25s ease-in;
+}
+
+.nav-link:hover,
+.nav-link:focus,
+.nav-link:active {
+  color: #6c9dce;
+}
+
+.burger {
+  cursor: pointer;
+  display: none;
+}
+
+.burger div {
+  background-color: #6c9dce;
+  border-radius: 50px;
+  height: 3px;
+  width: 25px;
+  margin: 5px;
+  transition: all 0.3s ease;
+}
+
+@media screen and (max-width: 1024px) {
+  .nav-links {
+    width: 60%;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  body {
+    overflow-x: hidden;
+  }
+
+  .nav-links {
+    background-color: #3db876;
+    position: absolute;
+    right: 0px;
+    height: 92vh;
+    top: 8vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 50%;
+    transform: translateX(100%);
+    transition: transform 0.5s ease-in;
+  }
+
+  .nav-link {
+    opacity: 0;
+  }
+
+  .burger {
+    display: block;
+  }
+}
+
+.nav-active {
+  transform: translateX(0%);
+}
+
+@keyframes navLinkFade {
+  from {
+    opacity: 0;
+    transform: translateX(50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+}
+
+.toggle .line1 {
+  transform: rotate(-45deg) translate(-5px, 6px);
+}
+
+.toggle .line2 {
+  opacity: 0;
+}
+
+.toggle .line3 {
+  transform: rotate(45deg) translate(-5px, -6px);
 }
 </style>

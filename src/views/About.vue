41e286bcfaadab3c350d1ed610b969d7d58ee677
Jackie Wong <wongjackie888@gmail.com>
2020-08-25 02:15:06 -1000

    Add profile image and update styling.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="about">
-    <h1>About Me</h1>
+    <header>
+      <h1>About Me</h1>
+    </header>
+    <main>
+      <div class="content">
+        <div class="bio">
+          <p>
+            Hi there! I'm Jackie, a self-learning developer based in Honolulu,
+            HI and am currently in my third year at the University of Hawaii at
+            Manoa, majoring in computer science. More importantly, I'm
+            passionate about exploring the possibilities of building cool
+            websites and scalable web apps that deliver a simple user interface
+            and modern user experience, while utilizing current backend
+            technologies when applicable.
+          </p>
+          <p>Technologies I've Worked With:</p>
+        </div>
+        <div class="profile-img">
+          <!-- <p>Insert Profile Image Here</p> -->
+          <img src="../assets/profile_img.jpg" alt="Profile Image" />
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -10,4 +32,48 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.about {
+  display: grid;
+  grid-template-rows: auto 1fr;
+}
+
+header h1 {
+  font-size: 32px;
+}
+
+.content {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  font-size: 18px;
+  text-align: left;
+}
+
+.content .bio {
+  padding: 7px 15px 10px 50px;
+  width: clamp(40ch, 50%, 50ch);
+}
+
+.content .profile-img {
+  padding-right: 5rem;
+}
+
+.content .profile-img::before {
+  content: '';
+  background-color: #1d99db;
+  border-radius: 50%;
+  opacity: 0.7;
+  position: absolute;
+  width: 360px;
+  height: 360px;
+  transform: translateX(10rem) translateY(60px);
+  z-index: -1;
+}
+
+.content img {
+  border-radius: 50%;
+  width: 360px;
+  height: 360px;
+}
+</style>

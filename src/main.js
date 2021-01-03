5647ce302a72acd090a2de4141665818aa60b4d5
Jackie Wong <wongjackie888@gmail.com>
2021-01-03 02:33:45 -1000

    Add event listener to window for navbar.
    -Make navbar position fixed on scroll.
    -Change navbar style on scroll.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
 }).$mount('#app');
+
+window.addEventListener('scroll', function() {
+  let nav = document.querySelector('nav');
+  nav.classList.toggle('fixed-nav', window.scrollY > 0);
+});

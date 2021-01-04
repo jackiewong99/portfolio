b9a44342565884d9d5c57a43ca18cf587935c547
Jackie Wong <wongjackie888@gmail.com>
2021-01-03 23:02:30 -1000

    Add routes for form submission success and fail.

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Contact from '../components/Contact.vue';
+import SubmissionFail from '../components/SubmissionFail.vue';
+import SubmissionSuccess from '../components/SubmissionSuccess.vue';
+
+Vue.use(Router);
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Contact',
+      component: Contact
+    },
+    {
+      path: '/thanks',
+      name: 'success',
+      component: SubmissionSuccess
+    },
+    {
+      path: '/404',
+      name: 'fail',
+      component: SubmissionFail
+    }
+  ]
+});
+
+export default router;
